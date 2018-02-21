d08a8ae5658cfbfc7d15f0bc0756ed2730e764f1
DESKTOP-GHHTF7T\ibrahim <ibrahim.uzul@gmail.com>
2018-02-21 21:51:21 +0300

    Lesson 62 : Class to object

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -10,7 +10,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
 }
 
 
